cf052321eddb1adf9ea66a0534eb69f2338bc3f9
peter <peterthomaskim@gmail.com>
2021-08-29 01:45:30 -0300

    typo

--- public/myjs.js
+++ public/myjs.js
@@ -1,13 +1,13 @@
-const cookie=document.querySelector(".cookie")
-const cookie_button=document.querySelector(".cookie-btn")
+const cookie = document.querySelector(".cookie")
+const cookie_button = document.querySelector(".cookie-btn")
 
-cookie_button.addEventListener("click",()=>{
+cookie_button.addEventListener("click", () => {
     cookie.classList.remove("active");
-    localStorage.setItem("cookieBanner","True")
+    localStorage.setItem("cookieBanner", "True")
 })
 
-setTimeout(()=>{
-    if(!localStorage.getItem("cookieBanner")){
+setTimeout(() => {
+    if (!localStorage.getItem("cookieBanner")) {
         cookie.classList.add("active")
     }
 }, 2000)
